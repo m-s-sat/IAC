31c55b96a509cfc4e933baff3f9aa3e2e937f3ed
m-s-sat <mrinalsatyarthi2005@gmail.com>
2025-10-02 14:08:40 +0530

    test workflow

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "172.20.0.1/16"
 
-  azs             = slice(data.aws_availability_zone.available.names, 0, 3)
+  azs             = slice(data.aws_availability_zone.available.name, 0, 3)
   private_subnets = ["172.20.0.1/24", "172.20.0.1/24", "172.20.0.1/24"]
   public_subnets  = ["172.20.0.1/24", "172.20.0.1/24", "172.20.0.1/24"]
 
